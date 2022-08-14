8efbc91ba3b02320f6056b6c5eb6aa6083604d14
Juan Cisneros <juanfrancistm2011@icloud.com>
2022-08-14 15:53:01 -0500

    MAIN CARD SWIPER

--- lib/widgets/card_swiper.dart
+++ lib/widgets/card_swiper.dart
@@ -1,14 +1,41 @@
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 
 class CardSwiper extends StatelessWidget{
-  const CardSwiper({Key? key}) : super(key: key);
+
+
 
   @override
   Widget build(BuildContext context) {
+
+    final size = MediaQuery.of(context).size; //size of the screen
+
     return Container(
-      width: double.infinity,
-      height: 0.6 * MediaQuery.of(context).size.height,
-      color: Colors.red,
+      width: double.infinity,//width of the screen
+      height: 0.5 * size.height,//0.6 is the ratio of the screen height
+      //color: Colors.red,
+      child: Swiper(
+        itemCount: 10,
+        layout: SwiperLayout.STACK,
+        itemWidth: 0.6 * size.width,
+        itemHeight: 0.4 * size.height,
+        itemBuilder: (BuildContext context, int index){
+          return GestureDetector(
+            onTap: (){
+              Navigator.pushNamed(context, 'details', arguments: 'movie-instace');
+            },
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20.0),
+              child: FadeInImage(
+                  placeholder:  AssetImage('assets/no-image.jpg'),
+                  image: NetworkImage('https://via.placeholder.com/300x400'),
+                  fit: BoxFit.cover,
+              ),
+            ),
+          );
+        }
+
+      ),
     );
   }
 }
